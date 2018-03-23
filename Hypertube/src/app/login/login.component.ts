96ee25d5b51d758cfd9aca98fee66aec2f702548
vlangman <vlangman00@gmail.com>
2018-03-23 16:07:08 +0200

    Merge remote-tracking branch 'origin/Lance' into Profile

--- Hypertube/src/app/login/login.component.ts
+++ Hypertube/src/app/login/login.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, NgZone } from '@angular/core';
+import { Component, OnInit, NgZone, OnDestroy } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router, ActivatedRoute } from "@angular/router";
 import { NgForm } from '@angular/forms';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Subscription } from 'rxjs';
 
 
 @Component({
@@ -10,11 +12,18 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 	editButton: boolean = false;
 	errormsg: string;
 	msg: string;
 	returnUrl: string;
+	email42: string;
+	username42: string;
+	photo42: string;
+	pass42: string;
+	sub42post: Subscription;
+	sub42get: Subscription;
+	// checkExist: number;
 	// loading = false;
 
 	constructor(
@@ -22,27 +31,34 @@ export class LoginComponent implements OnInit {
 		private router: Router,
 		private _ngZone: NgZone,
 		private route: ActivatedRoute,
-		private http: HttpClient
+		private http: HttpClient,
+		private db: AngularFirestore
 		// private socialAuth: SocialAuthService
 	) { }
 
 	ngOnInit() {
-		this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
-		console.log("this is hereerere");
-		console.log(this.returnUrl);
+		// this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+		// console.log("this is hereerere");
+		// console.log(this.returnUrl);
 
-		let provider = this.route.snapshot.params['provider'];
-		console.log("--------------");
-		console.log(this.route.snapshot.queryParams);
-		console.log(provider);
-		this.fourtytwo(this.route.snapshot.queryParams['code']);
-	}
-	this42test(test) {
-		console.log("wyetwyetwyetwyetwy");
+		// let provider = this.route.snapshot.params['provider'];
+		// console.log("--------------");
+		// console.log(this.route.snapshot.queryParams);
+		// console.log(provider);
+		if (this.route.snapshot.queryParams['code']) {
+			this.fourtytwo(this.route.snapshot.queryParams['code']);
+		}
+		// else {
+		// 	console.log("no query");
+		// }
 
 	}
+	// this42test(test) {
+	// 	console.log("wyetwyetwyetwyetwy");
+
+	// }
 	fourtytwo(code: string) {
-		console.log(code);
+		// console.log(code);
 		const params = {
 			code: code,
 			grant_type: 'authorization_code',
@@ -50,10 +66,10 @@ export class LoginComponent implements OnInit {
 			client_secret: '71ae6d11e5da5bdd03c9dcae3afe961c16089038137df2da7875ebc33d33f820',
 			redirect_uri: 'http://localhost:4200/Login'
 		};
-		console.log("____________________________________==");
-		return this.http.post('https://api.intra.42.fr/oauth/token', params)
+		// console.log("____________________________________==");
+		return this.sub42post = this.http.post('https://api.intra.42.fr/oauth/token', params)
 			.subscribe((res) => {
-				console.log(res);
+				// console.log(res);
 				// const jwtDecode = require('jwt-decode');
 				// const decoded = jwtDecode('eyJhbGciOiJIUzM4NCIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.' + res['access_token']);
 				// console.log(decoded);
@@ -61,9 +77,59 @@ export class LoginComponent implements OnInit {
 				// console.log(test);
 				const graphApiUrl = 'https://api.intra.42.fr/v2/me?access_token=' + res['access_token'];
 				let headers = { headers: new HttpHeaders({ 'content-Type': 'application/vnd.api+json' }) };
-				this.http.get(graphApiUrl, headers).subscribe((data) => {
-					const email = data['data']['attributes']['email'];
-					console.log(email);
+				this.sub42get = this.http.get(graphApiUrl, headers).subscribe((data) => {
+					this.email42 = data['data']['attributes']['email'];
+					this.username42 = data['data']['attributes']['login'];
+					this.photo42 = data['data']['attributes']['image-url'];
+					this.pass42 = data['data']['id'] + data['data']['attributes']['last-name'];
+					// console.log(this.pass42);
+					this.authService._firebaseAuth.auth.fetchProvidersForEmail(this.email42).then((providers) => {
+						if (providers.length > 0) {
+							this.authService.signInWithEmailAndPassword(this.email42, this.pass42).then((res) => {
+								this.router.navigate(['/Profile']);
+							})
+						} else {
+							this.authService.createUserWithEmailAndPassword(this.email42, this.pass42).then((res) => {
+								this.authService.updateProfile(this.username42, this.photo42)
+								this.db.collection("Users").doc(this.username42).set({
+									username: this.username42
+								}).then((res) => {
+									// console.log("added");
+								}).catch((err) => {
+									this.errormsg = err;
+									console.log(err);
+								});
+								// this.msg = this.authService.msg;
+
+							}).catch((err) => {
+								if (err.code != 'auth/email-already-in-use')
+									console.log(err);
+							});
+						}
+					})
+					// console.log(this.checkExist)
+					// this.authService.createUserWithEmailAndPassword(this.email42, this.pass42).then((res) => {
+					// 	this.authService.updateProfile(this.username42, this.photo42)
+					// 	this.db.collection("Users").doc(this.username42).set({
+					// 		username: this.username42
+					// 	}).then((res) => {
+					// 		// console.log("added");
+					// 	}).catch((err) => {
+					// 		this.errormsg = err;
+					// 		console.log(err);
+					// 	});
+					// 	// this.msg = this.authService.msg;
+
+					// }).catch((err) => {
+					// 	if (err.code === 'auth/email-already-in-use') {
+					// 		this.authService.signInWithEmailAndPassword(this.email42, this.pass42).then((res) => {
+					// 			this.router.navigate(['/Profile']);
+					// 		})
+					// 	}
+					// 	this.errormsg = err;
+					// 	if (err.code != 'auth/email-already-in-use')
+					// 		console.log(err);
+					// });
 				})
 
 				// if (err) return res.status(500).json(err);
@@ -81,10 +147,12 @@ export class LoginComponent implements OnInit {
 	}
 	facebookLogin() {
 		this.authService.signInWithFacebook()
-			.then((res) => {
-				this.router.navigate(['/'])
-			})
-			.catch((err) => console.log(err));
+			.catch((err) => {
+				if (err.code === 'auth/account-exists-with-different-credential') {
+					window.alert(err)
+				}
+				// console.log(err);
+			});
 	}
 
 	googleLogin() {
@@ -123,4 +191,10 @@ export class LoginComponent implements OnInit {
 		// console.log(this.editButton);
 	}
 
+	ngOnDestroy() {
+		if (this.sub42post)
+			this.sub42post.unsubscribe();
+		if (this.sub42get)
+			this.sub42get.unsubscribe();
+	}
 }
--- Hypertube/src/app/movies/movies.component.ts
+++ Hypertube/src/app/movies/movies.component.ts
@@ -5,7 +5,7 @@ import { MOVIES } from "../models/movies.model";
 import { YTS } from "../models/yts.model";
 import { Subscription } from "rxjs/Subscription";
 import 'rxjs/add/operator/map';
-import { ErrorObservable } from 'rxjs/observable/ErrorObservable';
+import { ErrorObservable  } from 'rxjs/observable/ErrorObservable';
 
 
 @Component({
@@ -30,7 +30,7 @@ export class MoviesComponent implements OnInit, OnDestroy {
 
 	//subscriptions to services that will be destroyed onDestroy
 	routerParamsSub: Subscription;
-	getNextPageSub : Subscription;
+	getNextPageSub: Subscription;
 	movieGenreSub: Subscription;
 	searchMovieSub: Subscription;
 	getMovieSub: Subscription;
@@ -42,9 +42,8 @@ export class MoviesComponent implements OnInit, OnDestroy {
 		private movieService: MovieService,
 		private route: ActivatedRoute,
 		private router: Router,
-	)
-	{
-		
+	) {
+
 	}
 
 	ngOnInit() {
@@ -56,8 +55,7 @@ export class MoviesComponent implements OnInit, OnDestroy {
 		console.log('creating Movies component');
 
 		this.routerParamsSub = this.route.params.subscribe((params) => {
-			if (params['Search'] && params['query_term'])
-			{
+			if (params['Search'] && params['query_term']) {
 				console.log('loading Search Movies');
 				this.displayLoad = true;
 				this.Movies = [];
@@ -70,25 +68,32 @@ export class MoviesComponent implements OnInit, OnDestroy {
 							this.displayLoad = false;
 							this.movieError = ret['error']['message'];
 						}
+<<<<<<< HEAD
 						else{
 							this.movieError = null;
+=======
+						else {
+>>>>>>> origin/Lance
 							console.log('Bottom')
 							console.log(ret);
 							this.Movies = ret;
 							this.movieType = params['query_term'];
-							this.displayLoad = false;	
+							this.displayLoad = false;
 						}
-						
+
 					}, (err) => {
+<<<<<<< HEAD
 						console.log(err);
 						this.movieError = err['message'];
 						this.displayLoad = false;
+=======
+
+>>>>>>> origin/Lance
 					}, () => {
 						this.searchMovieSub.unsubscribe();
 					}
 				)
-			} else if (params['genreId'])
-			{
+			} else if (params['genreId']) {
 				console.log('loading Movies by Genre');
 				this.displayLoad = true;
 				this.Movies = [];
@@ -111,8 +116,7 @@ export class MoviesComponent implements OnInit, OnDestroy {
 			}
 		})
 
-		if (!this.searchMode && !this.genreMode)
-		{
+		if (!this.searchMode && !this.genreMode) {
 			console.log('searching for FEATURED');
 			this.Movies = [];
 			this.getMovieSub = this.movieService.getMovies().subscribe(
@@ -131,7 +135,7 @@ export class MoviesComponent implements OnInit, OnDestroy {
 			)
 		}
 	}
-		
+
 
 	//autoloading function called when scrollbar near bottom of page
 	onScrollDown() {
@@ -139,6 +143,7 @@ export class MoviesComponent implements OnInit, OnDestroy {
 			console.log('Loading nextpage');
 			console.log(this.selectedGenre);
 			this.loadMore = true;
+<<<<<<< HEAD
 			this.getNextPageSub = this.movieService.getNextPage(this.page += 1)
 			.subscribe(
 				(res) => {
@@ -151,11 +156,20 @@ export class MoviesComponent implements OnInit, OnDestroy {
 					this.loadMore = false;
 				}
 			)
+=======
+			this.getNextPageSub = this.movieService.getNextPage(this.page += 1).subscribe(
+				(data: MOVIES[]) => {
+					console.log(this.page);
+					this.Movies = data;
+					this.loadMore = false;
+				})
+>>>>>>> origin/Lance
 		}
 		else if (this.selectedGenre && !this.searchMode && !this.displayLoad) {
 			console.log('Loading genre nextpage');
 			this.loadMore = true;
 			this.getNextMoviePageSub = this.movieService.getGenreNext(this.page += 1)
+<<<<<<< HEAD
 			.subscribe(
 				(res) => {
 					this.Movies = res;
@@ -167,6 +181,12 @@ export class MoviesComponent implements OnInit, OnDestroy {
 					this.loadMore = false;
 				}
 			)
+=======
+				.subscribe((data: MOVIES[]) => {
+					this.Movies = data;
+					this.loadMore = false;
+				})
+>>>>>>> origin/Lance
 		}
 	}
 
@@ -176,8 +196,20 @@ export class MoviesComponent implements OnInit, OnDestroy {
 	}
 
 
+<<<<<<< HEAD
 	viewMovie(id: number){
 		this.router.navigate(["Movies/Details", id]);
+=======
+	viewMovie(id: number, torrentData: {}) {
+		console.log(torrentData);
+		const quality = torrentData['quality'];
+		if (quality == "720p") {
+			this.router.navigate(["Movies/Details", id, torrentData['hash'], 720, { watch: true }]);
+		} else if (quality == "1080p") {
+
+			this.router.navigate(["Movies/Details", id, torrentData['hash'], 1080, { watch: true }]);
+		}
+>>>>>>> origin/Lance
 	}
 
 
@@ -185,11 +217,11 @@ export class MoviesComponent implements OnInit, OnDestroy {
 		this.hoverMovie = hoverId;
 	}
 
-	ngOnDestroy(){
+	ngOnDestroy() {
 		console.log('Destroy movies Component');
 		if (this.routerSub)
 			this.routerSub.unsubscribe();
-		if(this.routerParamsSub)
+		if (this.routerParamsSub)
 			this.routerParamsSub.unsubscribe();
 		if (this.getNextMoviePageSub)
 			this.getNextMoviePageSub.unsubscribe();
